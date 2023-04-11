9a638520078f6c985c97fac0731d5edcaaf48cf3
DESKTOP-T2D8UUS\ira <ira.zvonova@outlook.com>
2023-04-11 13:32:52 +0300

    adaptive header, promo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 2,
